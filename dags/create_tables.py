2d94a58abdd96a80e7f3ff28aea519d36503b7ed
Caphace-Ethan <ethanyoung135@gmail.com>
2021-09-30 18:43:24 +0300

    Updated: DAG script to schedule scripts for creating postgres db and tables, and running the migrations

--- dags/create_tables.py
+++ dags/create_tables.py
@@ -3,18 +3,31 @@ from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 import time
+import os
 
 
 def _create_db_table():
-	# Execute dbt to compile models
-	time.sleep(15)
-	return "Created table"
+	try:
+		project_dir = os.system("cd ..")
+		print(project_dir)
+		project_dir = os.system("cd scripts/")
+		os.system("./create_db_table.sh")
+		e = "Creating db and tables in PostgreSQL database, successfully"
+	except Exception as e:
+		e = e
+	return e
 
 
-def _load_data_db_table():
-	# Execute dbt to load the data to db
-	time.sleep(20)
-	return "Data Loaded to table"
+def _migrate_data_mysql_postgres():
+	try:
+		project_dir = os.system("cd ..")
+		print(project_dir)
+		project_dir = os.system("cd scripts/")
+		os.system("./migrate.sh")
+		e = "Migrating data from MySQL to PostgreSQL database, successfully"
+	except Exception as e:
+		e = e
+	return e
 
 
 with DAG("create_tables", start_date=datetime(2021, 9, 21),
@@ -27,7 +40,7 @@ with DAG("create_tables", start_date=datetime(2021, 9, 21),
 
 	loading_station_data_db = PythonOperator(
 		task_id="load_station_data",
-		python_callable=_load_data_db_table
+		python_callable=_migrate_data_mysql_postgres
 	)
 
 	creating_db_tables >> loading_station_data_db
