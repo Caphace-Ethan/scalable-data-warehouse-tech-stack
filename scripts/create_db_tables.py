766891e4bd068a39549c6935d4eceb03f2dbb3f3
Caphace-Ethan <ethanyoung135@gmail.com>
2021-09-30 17:56:43 +0300

    Updated: create_db_tables script

--- scripts/create_db_tables.py
+++ scripts/create_db_tables.py
@@ -5,6 +5,8 @@ global station_sqlFile
 global sensor_sqlFile
 station_sqlFile = './sql_schema/station_table_schema.sql'
 sensor_sqlFile = './sql_schema/sensor_table_schema.sql'
+station_data_path = "./data/I80_stations.csv"
+sensor_data_path = "./data/all_sensor_data1.csv"
 
 
 def DBConnect(dbName=None):
@@ -45,7 +47,7 @@ def createDB(dbName: str) -> None:
 def createTables(dbName: str, tableName: str) -> None:
 
     conn, cur = DBConnect(dbName)
-    cur.execute("DROP TABLE IF EXISTS stations")
+    cur.execute(f"DROP TABLE IF EXISTS {tableName};")
     fd = open(tableName, 'r')
     readSqlFile = fd.read()
     fd.close()
@@ -66,65 +68,14 @@ def createTables(dbName: str, tableName: str) -> None:
     return print(response)
 
 
-def insert_data_to_table(dbName: str, tableName: str) -> None:
-    try:
-        conn, cur = DBConnect(dbName)
-        df = pd.read_csv("./data/I80_stations.csv")
-        df = df.where((pd.notnull(df)), None)
-        df = df.astype(object).where(pd.notnull(df), None)
-
-        print(df.dtypes)
-        # df.to_sql(tableName, con=conn, index=False, if_exists='append')
-        for i, row in df.iterrows():
-            sql = "INSERT INTO sensor_data.stations VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-            cur.execute(sql, tuple(row))
-            print("Record inserted")
-            # the connection is not autocommitted by default, so we
-
-        conn.commit()
-        print("Inserted Records, Sucessfully")
-    except Exception as e:
-        print(":::", e)
-
-    conn.commit()
-    cur.close()
-    print("----- Done")
-
-
-def fetch_data_from_table(dbName: str, tableName: str) -> None:
-    try:
-        conn, cur = DBConnect(dbName)
-
-        sql = "SELECT * FROM stations"
-        cur.execute(sql)
-        print("Record inserted")
-        # Fetch all the records
-        result = cur.fetchall()
-        for i in result:
-            print(":::", i)
-
-        conn.commit()
-        print("Records Fetched, Sucessfully")
-    except Exception as e:
-        print(":::", e)
-
-    conn.commit()
-    cur.close()
-    print("----- Done")
-
 if __name__ == "__main__":
 
     if (True):
-        # createDB(dbName='sensor_data')
+        createDB(dbName='sensor_data')
         createTables(dbName='sensor_data', tableName=station_sqlFile)
-        # createTables(dbName='sensor_data', tableName=sensor_sqlFile)
+        createTables(dbName='sensor_data', tableName=sensor_sqlFile)
         print("Sucessfully, created db and Tables")
 
-    elif (False):
-        insert_data_to_table('sensor_data', 'stations')
-
-    elif (False):
-        fetch_data_from_table('sensor_data', 'stations')
 
 
 
